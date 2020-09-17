aa8c88ec984ea803cb3d128bf201d5c09f1ba61a
siddharth <siddharth.chauhan2@oyorooms.com>
2020-09-17 15:17:03 +0530

    Updates

---
+++ precache-manifest.3c8c4d49045b4c402bce18cf3bd8502c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c214ec1d1651dd1464b79c29945ca55",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0af3ddf32bccab4b6a9c",
+    "url": "/weather-app/static/js/2.27f6963c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.27f6963c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4dd51a19ad08fc4e02",
+    "url": "/weather-app/static/js/main.e3756725.chunk.js"
+  },
+  {
+    "revision": "f38f80fb6976f93468e3",
+    "url": "/weather-app/static/js/runtime-main.975b9fb1.js"
+  }
+]);
